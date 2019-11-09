8d0a94ea8c542a4a7c488f42a437732cc36842a7
jneilliii <jneilliii+github@gmail.com>
2019-11-09 14:20:18 -0500

    test

--- octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
+++ octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
@@ -32,7 +32,8 @@ $(function() {
 			let template = '<div class="btn btn-mini" data-bind="click: function() { if ($root.loginState.isUser()) { $root.open_thumbnail($data) } else { return; } }, css: {hidden: name.indexOf(\'.ufp.gcode\') < 0}" title="Show Thumbnail"><i class="fa fa-image"></i></div>';
 
 			$("#files_template_machinecode").text(function () {
-				$(this).prepend('<div class="inline_thumbnail" data-bind="if: $root.settingsViewModel.settings.plugins.UltimakerFormatPackage.inline_thumbnail() == true"><img data-bind="visible: $root.settingsViewModel.settings.plugins.UltimakerFormatPackage.inline_thumbnail() == true,attr: {src: inline_thumbnail_url()}" width="100%" style="display: none;"/></div>');
+				//$(this).prepend('<div class="inline_thumbnail" data-bind="if: $root.settingsViewModel.settings.plugins.UltimakerFormatPackage.inline_thumbnail() == true"><img data-bind="visible: $root.settingsViewModel.settings.plugins.UltimakerFormatPackage.inline_thumbnail() == true,attr: {src: inline_thumbnail_url()}" width="100%" style="display: none;"/></div>');
+				$(this).prepend('<div class="row-fluid"><img data-bind="attr: {src: $root.inline_thumbnail_url($data)}" width="100%"/></div>');
 				return $(this).text().replace(regex, '<div class="btn-group action-buttons">$1	' + template + '></div>');
 			});
 		});
