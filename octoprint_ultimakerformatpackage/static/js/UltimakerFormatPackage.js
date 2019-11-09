0606011459e92875e105ae916309ba55054a200d
jneilliii <jneilliii+github@gmail.com>
2019-11-09 14:39:40 -0500

    add conditional binding

--- octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
+++ octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
@@ -30,7 +30,7 @@ $(function() {
 		$(document).ready(function(){
 			let regex = /<div class="btn-group action-buttons">([\s\S]*)<.div>/mi;
 			let template = '<div class="btn btn-mini" data-bind="click: function() { if ($root.loginState.isUser()) { $root.open_thumbnail($data) } else { return; } }, css: {hidden: name.indexOf(\'.ufp.gcode\') < 0}" title="Show Thumbnail"><i class="fa fa-image"></i></div>';
-			let inline_thumbnail_template = '<div class="row-fluid"><img data-bind="attr: {src: $root.inline_thumbnail_url($data)}" width="100%"/></div>'
+			let inline_thumbnail_template = '<div class="row-fluid" data-bind="if: $data.name.indexOf(\'.ufp.gcode\') > -1"><img data-bind="attr: {src: $root.inline_thumbnail_url($data)}, visible: $data.name.indexOf(\'.ufp.gcode\') > -1" width="100%" style="display: none;"/></div>'
 
 			$("#files_template_machinecode").text(function () {
 				var return_value = inline_thumbnail_template + $(this).text();
