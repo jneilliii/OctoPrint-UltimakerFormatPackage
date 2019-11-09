3dea672f2909643ea6f51e1886bcba68566549c3
jneilliii <jneilliii+github@gmail.com>
2019-11-09 15:57:47 -0500

    test

--- octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
+++ octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
@@ -9,7 +9,6 @@ $(function() {
 		var self = this;
 		self.thumbnail_url = ko.observable('/static/img/tentacle-20x20.png');
 		self.thumbnail_title = ko.observable('');
-		self.inline_thumbnail = ko.observable();
 
 		self.settingsViewModel = parameters[0];
 		self.filesViewModel = parameters[1];
@@ -28,23 +27,24 @@ $(function() {
 			return '/plugin/UltimakerFormatPackage/' + data.name.replace('.ufp.gcode','.png');
 		}
 
+		self.filesViewModel.inline_thumbnail = ko.observable(false);
+
 		self.onBeforeBinding = function() {
-			console.log(self.settingsViewModel);
+			self.filesViewModel.inline_thumbnail(self.settingsViewModel.plugins.UltimakerFormatPackage.inline_thumbnail());
 		}
 
 		$(document).ready(function(){
-			self.inline_thumbnail(self.settingsViewModel.plugins.UltimakerFormatPackage.inline_thumbnail());
 			let regex = /<div class="btn-group action-buttons">([\s\S]*)<.div>/mi;
 			let template = '<div class="btn btn-mini" data-bind="click: function() { if ($root.loginState.isUser()) { $root.open_thumbnail($data) } else { return; } }, css: {hidden: name.indexOf(\'.ufp.gcode\') < 0}" title="Show Thumbnail"><i class="fa fa-image"></i></div>';
-			let inline_thumbnail_template = '<div class="row-fluid" data-bind="if: $data.name.indexOf(\'.ufp.gcode\') > -1"><img data-bind="attr: {src: $root.inline_thumbnail_url($data)}, visible: $data.name.indexOf(\'.ufp.gcode\') > -1, click: function() { if ($root.loginState.isUser()) { $root.open_thumbnail($data) } else { return; } }" width="100%" style="display: none;"/></div>'
+			let inline_thumbnail_template = '<div class="row-fluid" data-bind="if: ($data.name.indexOf(\'.ufp.gcode\') > -1 && $root.inline_thumbnail() == true)"><img data-bind="attr: {src: $root.inline_thumbnail_url($data)}, visible: ($data.name.indexOf(\'.ufp.gcode\') > -1 && $root.inline_thumbnail() == true), click: function() { if ($root.loginState.isUser()) { $root.open_thumbnail($data) } else { return; } }" width="100%" style="display: none;"/></div>'
 
 			$("#files_template_machinecode").text(function () {
 				var return_value = '';
-				if(self.settingsViewModel.plugins.UltimakerFormatPackage.inline_thumbnail() == true){
+				//if(self.settingsViewModel.plugins.UltimakerFormatPackage.inline_thumbnail() == true){
 					return_value = inline_thumbnail_template + $(this).text();
-				} else {
+				//} else {
 					return_value = return_value.replace(regex, '<div class="btn-group action-buttons">$1	' + template + '></div>');
-				}
+				//}
 				return return_value
 			});
 		});
