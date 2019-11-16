db374d8974c7ecf52f855cdcacc3af3ad83d01fc
jneilliii <jneilliii+github@gmail.com>
2019-11-15 22:05:48 -0500

    update readme with screenshots

--- octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
+++ octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
@@ -15,6 +15,7 @@ $(function() {
 		self.filesViewModel = parameters[1];
 
 		self.filesViewModel.open_thumbnail = function(data) {
+			console.log(data);
 			if(data.name.indexOf('.ufp.gcode') > 0){
 				var thumbnail_title = data.name.replace('.ufp.gcode','.ufp');
 				var thumbnail_url = '/plugin/UltimakerFormatPackage/thumbnail/' + data.name.replace('.ufp.gcode','.png');
