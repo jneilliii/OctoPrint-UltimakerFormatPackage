d843f92217247eb0802dd3119d35d1b5f06b4899
jneilliii <jneilliii+github@gmail.com>
2019-11-16 01:36:12 -0500

    0.1.1
    Updated readme, fixed issues related to spaces in filenames, added sub-folder support.

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # OctoPrint-UltimakerFormatPackage
 
-This plugin adds support for Ultimaker Format Package (.ufp) files. Ultimaker Format Package files are based on Open Packaging Conventions (OPC) and contain compressed gcode and a preview thumbnail. The preview thumbnail can be shown in OctoPrint from the files list.
+This plugin adds support for Ultimaker Format Package (.ufp) files. Ultimaker Format Package files are based on Open Packaging Conventions (OPC) and contain compressed gcode and a preview thumbnail. This format will automatically be used by the [OctoPrint Connection](https://github.com/fieldOfView/Cura-OctoPrintPlugin) plugin in Cura (install via Marketplace) if this plugin is installed.
+
+The preview thumbnail can be shown in OctoPrint from the files list by clicking the newly added image button.
+
+![button](screenshot_button.png)
+
+The thumbnail will open in a modal window.
+
+![thumbnail](screenshot_thumbnail.png)
+
+If enabled in settings the thumbnail can also be embedded as an inline thumbnail within the file list itself. If you use this option it's highly recommended to use Themify to make the file list taller and/or adjust the thumbnail's size.  The css selector for setting the file list height should be `#files > div > div.gcode_files > div.scroll-wrapper` and the css selector for the image should be `div.row-fluid.inline_thumbnail > img`.  For example to make a 100px wide centered image in the file list create the following css rules.
+
+| Selector                                            | CSS-Rule     | Value |
+|-----------------------------------------------------|--------------|-------|
+| #files > div > div.gcode_files > div.scroll-wrapper | height       | 800px |
+| div.row-fluid.inline_thumbnail > img                | width        | 100px |
+| div.row-fluid.inline_thumbnail > img                | display      | block |
+| div.row-fluid.inline_thumbnail > img                | margin-left  | auto  |
+| div.row-fluid.inline_thumbnail > img                | margin-right | auto  |
+
+![thumbnail](screenshot_inline_thumbnail.png)
 
 ## Setup
 
@@ -8,3 +28,8 @@ Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wik
 or manually using this URL:
 
     https://github.com/jneilliii/OctoPrint-UltimakerFormatPackage/archive/master.zip
+
+## Support My Efforts
+I programmed this plugin for fun and do my best effort to support those that have issues with it, please return the favor and support me.
+
+[![paypal](paypal-with-text.png)](https://paypal.me/jneilliii)
--- octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
+++ octoprint_ultimakerformatpackage/static/js/UltimakerFormatPackage.js
@@ -9,26 +9,34 @@ $(function() {
 		var self = this;
 		self.thumbnail_url = ko.observable('/static/img/tentacle-20x20.png');
 		self.thumbnail_title = ko.observable('');
+		self.inline_thumbnail = ko.observable();
 
 		self.settingsViewModel = parameters[0];
 		self.filesViewModel = parameters[1];
 
 		self.filesViewModel.open_thumbnail = function(data) {
 			if(data.name.indexOf('.ufp.gcode') > 0){
-				var thumbnail_title = data.name.replace('.ufp.gcode','.ufp');
-				var thumbnail_url = '/plugin/UltimakerFormatPackage/' + data.name.replace('.ufp.gcode','.png');
+				var thumbnail_title = data.path.replace('.ufp.gcode','.ufp');
+				var thumbnail_url = '/plugin/UltimakerFormatPackage/thumbnail/' + data.path.replace('.ufp.gcode','.png');
 				self.thumbnail_url(thumbnail_url);
 				self.thumbnail_title(thumbnail_title);
 				$('div#thumbnail_viewer').modal("show");
 			}
 		}
 
+		self.filesViewModel.inline_thumbnail_url = function(data) {
+			return '/plugin/UltimakerFormatPackage/thumbnail/' + data.path.replace('.ufp.gcode','.png');
+		}
+
 		$(document).ready(function(){
 			let regex = /<div class="btn-group action-buttons">([\s\S]*)<.div>/mi;
-			let template = '<div class="btn btn-mini" data-bind="click: function() { if ($root.loginState.isUser()) { $root.open_thumbnail($data) } else { return; } }, css: {hidden: name.indexOf(\'.ufp.gcode\') < 0}" title="Show Thumbnail"><i class="fa fa-image"></i></div>';
+			let template = '<div class="btn btn-mini" data-bind="click: function() { if ($root.loginState.isUser()) { $root.open_thumbnail($data) } else { return; } }, visible: ($data.name.indexOf(\'.ufp.gcode\') > -1 && $root.settingsViewModel.settings.plugins.UltimakerFormatPackage.inline_thumbnail() == false)" title="Show Thumbnail" style="display: none;"><i class="fa fa-image"></i></div>';
+			let inline_thumbnail_template = '<div class="row-fluid inline_thumbnail" data-bind="if: ($data.name.indexOf(\'.ufp.gcode\') > -1 && $root.settingsViewModel.settings.plugins.UltimakerFormatPackage.inline_thumbnail() == true)"><img data-bind="attr: {src: $root.inline_thumbnail_url($data)}, visible: ($data.name.indexOf(\'.ufp.gcode\') > -1 && $root.settingsViewModel.settings.plugins.UltimakerFormatPackage.inline_thumbnail() == true), click: function() { if ($root.loginState.isUser()) { $root.open_thumbnail($data) } else { return; } }" width="100%" style="display: none;"/></div>'
 
 			$("#files_template_machinecode").text(function () {
-				return $(this).text().replace(regex, '<div class="btn-group action-buttons">$1	' + template + '></div>');
+				var return_value = inline_thumbnail_template + $(this).text();
+				return_value = return_value.replace(regex, '<div class="btn-group action-buttons">$1	' + template + '></div>');
+				return return_value
 			});
 		});
 	}
