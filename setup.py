31a34b3009004c4de2a823603051915d0a2586b7
jneilliii <jneilliii+github@gmail.com>
2020-11-30 18:10:56 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 plugin_identifier = "UltimakerFormatPackage"
 plugin_package = "octoprint_ultimakerformatpackage"
 plugin_name = "OctoPrint-UltimakerFormatPackage"
-plugin_version = "0.2.1rc1"
+plugin_version = "0.2.1"
 plugin_description = """Plugin that allows the upload of Cura exported ufp file format."""
 plugin_author = "jneilliii"
 plugin_author_email = "jneilliii+github@gmail.com"
